6ba18311e9006c8ee0c51608235b708c7230f0bd
artem712 <artemLarch77@gmail.com>
2020-07-13 17:15:42 +0530

    added index.html

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'inventory/index.html')
+
