552db0a9578beed79bceecf84fad008161d864f5
artem712 <artemLarch77@gmail.com>
2020-07-16 15:14:50 +0530

    add button

--- inventory/urls.py
+++ inventory/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('employee/',views.employee,name='employee'),
+    path('add_employee/',views.add_employee,name='add_employee'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from .models import Employee
+from .forms import *
 
 # Create your views here.
 
@@ -20,3 +21,13 @@ def employee(request):
 	Emps = Employee.objects.all()
 	return render(request, 'inventory/employee.html', { 'Emps': Emps })    
 
+def add_employee(request):
+	if request.method=="POST":
+		form=EmployeeForm(request.POST)
+
+		if form.is_valid():
+			form.save()
+			return redirect('employee')
+	else:
+		form=EmployeeForm()
+		return render(request,'add_employee.html',{'forms': form})
