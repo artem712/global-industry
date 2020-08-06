ee4ded40c49090fdf012b9990db9908eb48e2c0b
artem712 <artemLarch77@gmail.com>
2020-08-07 00:06:34 +0530

    More works done

--- inventory/models.py
+++ inventory/models.py
@@ -2,25 +2,47 @@ import datetime
 from django.utils.timezone import now
 from django.db import models
 from django.utils import timezone
+from django import forms
 
 # Create your models here.
 
-class Products(models.Model):
-	name = models.CharField("Product Name", max_length=30)
-	cost = models.DecimalField(max_digits=10, decimal_places=2) # cost per Kg
-	wages = models.DecimalField(max_digits=10, decimal_places=2) # wages per Kg
-	weight = models.DecimalField(max_digits=10, decimal_places=2) # weight of the products in kg
+class raw_materials(models.Model): # commpany Stock 
+	name 	 = models.CharField("Material Name", max_length=30, default="None")
+	cost 	 = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Cost of the material")
+	weight 	 = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Currently Available")
+	make     = models.DecimalField(max_digits=4, decimal_places=2, verbose_name="Product Make percentage")
 
-	def is_available(self, w): # use for reduce_product 
+	def __str__(self):
+		return self.name 
+
+	def getmake(self): # it will return 0-1 value for 0-100 percentage 
+		return self.make/100 
+
+	def is_available(self, w): # use before reduce
+		if(self.weight >= w/self.getmake()):
+			return True 
+		return False 
+
+	def reduce(self, w): # use for update work 
+		self.weight -= w/self.getmake() 
+		self.save() 
+
+	def update_weight(self, weight):
+		self.weight += weight 
+		self.save()
+
+class Products(models.Model):
+	name 	= models.CharField("Product Name", max_length=30)
+	cost 	= models.DecimalField(max_digits=10, decimal_places=2, default=0) # cost per Kg
+	wages 	= models.DecimalField(max_digits=10, decimal_places=2, default=0) # wages per Kg
+	weight 	= models.DecimalField(max_digits=10, decimal_places=2, default=0) # weight of the products in kg
+
+	def is_available(self, w): # use before reduce_product 
 		return ((self.weight - w) >= 0)
 
 	def reduce_product(self, w): # use for customer delivery  
-		if(self.is_available(w)):
-			self.weight -= w
-			self.save()
-		else:
-			print("There is less or no Product {}".format(self.name)) 
-			return
+		self.weight -= w
+		self.save()
 
 	def add_product(self, w): # Employee add products 
 		self.weight += w
@@ -42,8 +64,8 @@ class Employee(models.Model):
 	isPaid 		= models.BooleanField(default=False)
 	lastSalary 	= models.DateTimeField('last salary updated date', default=now, blank=True)
 
-	DESIGNATION_WORKER= 'Worker'
 	DESIGNATION_CEO= 'Manager'
+	DESIGNATION_WORKER= 'Worker'
 	DESIGNATION_SUPERVISER= 'Superviser'
 	DESIGNATION_MARKETING= 'MarketingHead'
 	DESIGNATION_OTHERS= 'Others'
@@ -81,28 +103,71 @@ class Salary(models.Model):
 	total 		= models.DecimalField(max_digits=10, decimal_places=2, default=0) # basic + bonus
 	paidDate 	= models.DateTimeField('Date of paided', default=now, blank=True)
 
+class Work(models.Model):
+	emp 	 = models.ForeignKey(Employee, on_delete=models.CASCADE)
+	product  = models.ForeignKey(Products, on_delete=models.CASCADE)
+	material = models.ForeignKey(raw_materials, on_delete=models.CASCADE)
+	weight 	 = models.DecimalField(max_digits=12, decimal_places=2,  default=0.0, blank=True)
+
+
+# ____________________ For Customer and Orders models ________________________
+
 class Customer(models.Model):
-	name = models.CharField("Customer Name", max_length=30, default=' ', blank=True)
-	address = models.CharField(max_length=70, default=' ', blank=True)
-	phone = models.CharField("Phone Number", max_length=11, default=' ', blank=True)
+	name 	= models.CharField("Customer Name", max_length=30, default='No Name', blank=True)
+	address = models.CharField(max_length=70, default='No Address', blank=True)
+	phone 	= models.CharField("Phone Number", max_length=11, default='No Number', blank=True)
 
 	def __str__(self):
 		return self.name ; 
 
-class Work(models.Model):
-	emp = models.ForeignKey(Employee, on_delete=models.CASCADE)
-	product = models.ForeignKey(Products, on_delete=models.CASCADE)
-	weight = models.DecimalField(max_digits=12, decimal_places=2,  default=0.0, blank=True)
 
 class Orders(models.Model):
-	cus = models.ForeignKey(Customer, on_delete=models.CASCADE)
-	product = models.ForeignKey(Products, on_delete=models.CASCADE)
-	Odate = models.DateTimeField('Date of Ordered', default=now, blank=True)
-	Ddate = models.DateTimeField('Delivered Date', default=now, blank=True)
+
+	"""
+	Contains a list of orders. 
+	One row per order. 
+	Each order is placed by a customer and has a Customer_ID - which can be used to link back to the customer record.
+	
+	"""
+	cus 		= models.ForeignKey(Customer, on_delete=models.CASCADE)
+	Odate 		= models.DateTimeField('Date of Ordered', default=now, blank=True)
+	Ddate 		= models.DateTimeField('Delivered Date', default=now, blank=True)
+	total_amt 	= models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Total Amount")
 	isDelivered = models.BooleanField(default=False)
 
+class OrderItems(models.Model):
+
+	"""
+	Contains a list of order items. 
+	One row for each item on an order - so each Order can generate multiple rows in this table. 
+	Each item ordered is a product from your inventory, so each row has a product_id, which links to the products table.
+	
+	"""
+	order 		= models.ForeignKey(Orders, on_delete=models.CASCADE)
+	product 	= models.ForeignKey(Products, on_delete=models.CASCADE)
+	weight		= models.DecimalField(max_digits=10, decimal_places=2) # weight of the products in kg
+
+
+# ____________________ For Supplier and Materials models ________________________
+  
+
+
 class Supplier(models.Model):
-	name = models.CharField("Supplier Name", max_length=30, default=' ', blank=True)
-	address = models.CharField(max_length=70, default=' ', blank=True)
-	phone = models.CharField("Phone Number", max_length=11, default=' ', blank=True)	
+	name 	= models.CharField("Supplier Name", max_length=30)
+	address = models.CharField(verbose_name="Address of the supplier",max_length=70, default='None', blank=True)
+	phone 	= models.CharField("Phone Number", max_length=11, default='0')
+
+	def __str__(self):
+		return self.name ;
+
+class materials_order(models.Model):
+	sup 	 = models.ForeignKey(Supplier, on_delete=models.CASCADE)	
+	material = models.ForeignKey(raw_materials, on_delete=models.CASCADE)	
+	weight 	 = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+
+
+
+
 
--- inventory/views.py
+++ inventory/views.py
@@ -14,10 +14,10 @@ from datetime import datetime, timedelta
 from django.db.models import Q
 from .models import *	
 from .forms import *
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def registerPage(request):
-
 	form=CreateUserForm()
 
 	if request.method=="POST":
@@ -46,6 +46,20 @@ def loginPage(request):
 			
 	return render(request,'inventory/login.html')
 
+def login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user= auth.authenticate(username=username,password=password)
+        if user is not None:
+        	auth.login(request, user)
+        	return render(request,'inventory/dashboard.html')
+        else:
+            return render(request,'inventory/register.html')	
+    else:
+      return render(request,'inventory/login.html')
+
 def logoutUser(request):
 	logout(request)
 	return redirect('inventory:login')
@@ -61,18 +75,21 @@ def dashboard(request):
 
 def employee(request):
 	Emps = Employee.objects.all()
-	return render(request, 'inventory/employee.html', { 'Emps': Emps })    
+	return render(request, 'inventory/employee.html', { 'Emps': Emps })   
 
 def add_employee(request):
+	form=EmployeeForm(request.POST or None, request.FILES or None)
 	if request.method=="POST":
 		form=EmployeeForm(request.POST)
-
 		if form.is_valid():
 			form.save()
+			messages.success(request, 'Employee Created.')
 			return redirect('/employee')
-	else:
-		form=EmployeeForm()
-		return render(request,'inventory/add_employee.html',{'form': form})
+		else:
+			messages.error(request, 'Employee Not Created.')
+			messages.error(request, form.errors)
+	header = "Create Employee here" 
+	return render(request,'inventory/add_common.html',{'form': form, 'header' : header })
 
 
 def emp_edit(request, emp_id):
@@ -81,6 +98,7 @@ def emp_edit(request, emp_id):
 		form = EmployeeForm(request.POST, instance=emp)
 		if form.is_valid():
 			form.save()
+			messages.success(request, '{} updated'.format(emp.name))
 			return redirect('/employee')
 	else:
 		form = EmployeeForm(instance=emp)
@@ -90,6 +108,7 @@ def emp_edit(request, emp_id):
 def delete_employee(request, emp_id):
 	Employee.objects.filter(id=emp_id).delete()
 	Emps = Employee.objects.all()
+	messages.warning(request, 'Employee Deleted.')
 	return render(request, 'inventory/employee.html', { 'Emps' : Emps } )
 
 def view_works(request, emp_id):
@@ -105,14 +124,30 @@ def add_work(request, emp_id):
 		if form.is_valid():
 			form = form.save(commit=False)
 			form.emp = emp 
-			form.product.add_product(form.weight)
-			emp.add_bonus( form.product.get_wages() * form.weight)
-			emp.save()
-			form.save()
-			return redirect('/employee')
-	else:
-		form = WorkForm(initial={'emp': emp })
-		return render(request, 'inventory/add_work.html', {'form' : form, 'emp' : emp })
+
+			if(form.material.is_available(form.weight)):
+				form.product.add_product(form.weight)
+				form.material.reduce(form.weight)
+				try:
+					raw_waste = Products.objects.get(name='raw_waste')
+				except ObjectDoesNotExist:
+					raw_waste = Products(name='raw_waste')
+
+				w = ( form.weight / form.material.getmake()) - form.weight 
+				raw_waste.add_product(w)
+				raw_waste.save()
+				emp.add_bonus(form.product.get_wages() * form.weight)
+				emp.save()
+				form.save()
+				messages.success(request, 'Work updated for {}'.format(emp))
+				return redirect('/employee')
+			else:
+				messages.error(request, 'There is no such amount of raw materials to make this product.')
+		messages.error(request, 'Work is Not updated for {}.'.format(emp))
+
+	header = "Add work for {}".format(emp)
+	form = WorkForm(initial={'emp': emp })
+	return render(request, 'inventory/add_common.html', {'form' : form, 'header' : header })
 
 
 # _____________________ For ProductS _______________________________
@@ -123,15 +158,18 @@ def product_details(request):
 	return render(request, 'inventory/product_details.html', { 'pro': pro })	
 
 def add_product(request):
+	form=ProductForm(request.POST or None, request.FILES or None)
 	if request.method=="POST":
 		form=ProductForm(request.POST)
-
 		if form.is_valid():
 			form.save()
-		return redirect('/product_details')
-	else:
-		form=ProductForm()
-		return render(request,'inventory/add_product.html',{'form': form})
+			messages.success(request, 'Product Created.')
+			return redirect('/product_details')
+		else:
+			messages.error(request, 'Product not Created.')
+			messages.error(request, form.errors)
+	header = 'Create new Product'
+	return render(request,'inventory/add_common.html',{'form': form, 'header' : header })
 
 def edit_product(request, pro_id):
 	pro = get_object_or_404(Products, pk=pro_id)
@@ -198,19 +236,22 @@ def salary_cal(request):
 
 
 def customer(request):
-	Cust = Customer.objects.all()
-	return render(request, 'inventory/customer.html', { 'Cust': Cust })	
+	cus = Customer.objects.all()
+	return render(request, 'inventory/customer.html', { 'cus': cus })	
 
 def add_customer(request):
+	form=CustomerForm(request.POST or None, request.FILES or None)
 	if request.method=="POST":
 		form=CustomerForm(request.POST)
-
 		if form.is_valid():
 			form.save()
+			messages.success(request, 'Customer Created.')
 			return redirect('/customer')
-	else:
-		form=CustomerForm()
-		return render(request,'inventory/add_customer.html',{'form': form})
+		else:
+			messages.error(request, 'Customer not Created.')
+			messages.error(request, form.errors)
+	header = 'Create customer' 
+	return render(request,'inventory/add_common.html',{'form': form, 'header' : header })
 
 def cust_edit(request, cus_id):
 	cus = get_object_or_404(Customer, pk=cus_id)
@@ -223,27 +264,74 @@ def cust_edit(request, cus_id):
 		form = CustomerForm(instance=cus)
 		return render(request, 'inventory/edit_cust.html', {'form': form, 'cus' : cus})
 
-
 def delete_customer(request, cus_id):
-	Customer.objects.filter(id=cus_id).delete()
-	Cus = Customer.objects.all()
-	return render(request, 'inventory/customer.html', { 'Cus' : Cus } )
-
-	
-				
-
-def new_order(request, cus_id):
 	cus = get_object_or_404(Customer, pk=cus_id)
-	orders = Orders.objects.filter(cus=cus_id)
+	messages.success(request, '{} is deleted'.format(cus))
+	cus.delete()
+	return redirect('/customer')
 
-	if request.method=="POST":
-		form=OrderForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect('/dashboard')
+# _____________________ For Orders _______________________________
+			
+def order_all(request):
+	return render(request, 'inventory/order.html', {'order' : Orders.objects.all() })
+
+def order_not_delivered(request):
+	return render(request, 'inventory/order.html', {'order' : Orders.objects.filter(isDelivered=False) })
+
+def order_delivered(request):
+	return render(request, 'inventory/order.html', {'order' : Orders.objects.filter(isDelivered=True) })
+
+def order_list(request, cus_id): # for particular customer 
+	cus = get_object_or_404(Customer, pk=cus_id)
+	order = Orders.objects.filter(cus=cus_id)
+	return render(request, 'inventory/order.html', {'order' : order})
+
+def order_details(request, ord_id): # particular order details 
+	items = OrderItems.objects.filter(order=ord_id)
+	return render(request, 'inventory/order_details.html', {'items' : items})	
+
+def order_now(request, cus_id):
+	if request.method == 'POST':
+		formset = OrderFormset(request.POST)
+		if formset.is_valid() :
+			cus 	= get_object_or_404(Customer, pk=cus_id)
+			total 	= 0 
+			order 	= Orders(cus=cus, total_amt=total, Odate=now())
+			order.save() 
+			for form in formset: 
+				product = form.cleaned_data.get('product')
+				weight 	= form.cleaned_data.get('weight')
+				total  += ( product.cost * weight ) 
+				items 	= OrderItems(order=order,product=product, weight=weight) 
+				items.save()
+			order.total_amt = total 
+			order.save() 
+			messages.success(request, 'Order Booked.')
+			return redirect('/customer')
+		else:
+			messages.error(request, 'Order Canceled.'.format(sup))
+			messages.error(request, formset.errors)
 	else:
-		form = OrderForm()
-		return render(request, 'inventory/order_info.html', {'form' : form, 'orders' : orders, 'cus' : cus })
+		formset = OrderFormset(request.GET or None)
+	return render(request, 'inventory/order_now.html', { 'formset': formset })
+
+def delivered(request, ord_id):
+	order 			  = get_object_or_404(Orders, pk=ord_id)
+	items 			  = OrderItems.objects.filter(order=order)
+	for i in items :
+		if(i.product.is_available(i.weight) == False):
+			messages.error(request, '{} is Out of Stock!'.format(i.product.name))
+			messages.error(request, 'Order cannot be deliver.')
+			return redirect('/order_all')
+
+	for i in items :
+		i.product.reduce_product(i.weight)
+
+	order.Ddate 	  = now()
+	order.isDelivered = True
+	order.save()
+	messages.success(request, 'Order Completed.')
+	return redirect('/order_all')
 
 
 # _____________________ For Supplier _______________________________
@@ -253,45 +341,97 @@ def supplier(request):
 	return render(request, 'inventory/supplier.html', { 'Sup': Sup })
 
 def add_supplier(request):
+	form=SupplierForm(request.POST or None, request.FILES or None)
 	if request.method=="POST":
 		form=SupplierForm(request.POST)
-
 		if form.is_valid():
 			form.save()
+			messages.success(request, 'Supplier Created.')
 			return redirect('/supplier')
-	else:
-		form=SupplierForm()
-		return render(request,'inventory/add_supplier.html',{'form': form})
+		else:
+			messages.error(request, 'Supplier not Created.')
+			messages.error(request, form.errors)
+	header = 'Add Supplier'
+	return render(request,'inventory/add_common.html',{'form': form, 'header' : header })
 
 def sup_edit(request, sup_id):
-	emp = get_object_or_404(Supplier, pk=sup_id)
+	sup = get_object_or_404(Supplier, pk=sup_id)
+	form = SupplierForm(instance=sup)
 	if request.method == "POST":
-		form = SupplierForm(request.POST, instance=emp)
+		form = SupplierForm(request.POST, instance=sup)
 		if form.is_valid():
 			form.save()
+			messages.success(request, '{} Modified.'.format(sup))
 			return redirect('/supplier')
-	else:
-		form = SupplierForm(instance=emp)
-		return render(request, 'inventory/edit_sup.html', {'form': form})	
+		else:
+			messages.error(request, '{} not altered.'.format(sup))
+			messages.error(request, form.errors)
+	header = "Modify {}".format(sup)
+	return render(request, 'inventory/add_common.html', {'form': form, 'header' : header })	
 
 def delete_supplier(request, sup_id):
-	Supplier.objects.filter(id=sup_id).delete()
-	Sup = Supplier.objects.all()
-	return render(request, 'inventory/supplier.html', { 'Sup' : Sup } )			
+	sup = get_object_or_404(Supplier, pk=sup_id)
+	name = sup.name
+	sup.delete()
+	messages.success(request, '{} deleted.'.format(name))
+	return redirect('/supplier')		
+
+def buy_material(request):
+	form=MaterialsOrderForm(request.POST or None, request.FILES or None)
+	if request.method=="POST":
+		form=MaterialsOrderForm(request.POST)
+		if form.is_valid():
+			material = form.cleaned_data.get('material')
+			weight 	 = form.cleaned_data.get('weight')
+			rm = get_object_or_404(raw_materials, name=material)
+			rm.update_weight(weight)
+			rm.save() 
+			form.save()
+			messages.success(request, '{} is boughted.'.format(material))
+			return redirect('/materials')
+		else:
+			messages.error(request, form.errors)
+	header = 'Buy '
+	return render(request,'inventory/add_common.html',{'form': form})
 
 
 
-def login(request):
-    if request.method=='POST':
-        username=request.POST['username']
-        password=request.POST['password']
+# _____________________ For Raw Materials _______________________________
 
-        user= auth.authenticate(username=username,password=password)
-        if user is not None:
-        	auth.login(request, user)
-        	return render(request,'inventory/dashboard.html')
-        else:
-            return render(request,'inventory/register.html')	
-    else:
-      return render(request,'inventory/login.html')
+def materials(request):
+	mat = raw_materials.objects.all()
+	return render(request, 'inventory/materials.html', { 'mat': mat })
 
+def add_material(request):
+	form=MaterialsForm(request.POST or None, request.FILES or None)
+	if request.method=="POST":
+		form=MaterialsForm(request.POST)
+		if form.is_valid():
+			form.save()
+			messages.success(request, 'Raw Material is Created.')
+			return redirect('/materials')
+		else:
+			messages.error(request, 'Material is not created')
+			messages.error(request,  form.errors)
+	header = "Add New Raw Material" 
+	return render(request,'inventory/add_common.html',{'form': form, 'header' : header })
+
+def material_edit(request, mat_id):
+	mat = get_object_or_404(raw_materials, pk=mat_id)
+	if request.method == "POST":
+		form = MaterialsForm(request.POST, instance=mat)
+		if form.is_valid():
+			form.save()
+			messages.success(request, '{} is updated'.format(mat))
+			return redirect('/materials')
+		else:
+			messages.error(request,  form.errors)
+	form 	= MaterialsForm(instance=mat)
+	header  = "Update {}".format(mat)   
+	return render(request, 'inventory/add_common.html', {'form': form, 'header' : header })
+
+def delete_material(request, mat_id):
+	mat = get_object_or_404(raw_materials, pk=mat_id)
+	messages.success(request, '{} is deleted'.format(mat))
+	mat.delete()
+	return redirect('/materials')
